9ef39337ca94551abcdb305d3c069771a7b0c4ab
Cole Busby <cole.busby@mariadb.com>
2021-09-24 16:05:52 -0700

    basic 'install' script is functional. Migrated to a new version for marketing purposes.

--- install.sh
+++ install.sh
@@ -2,42 +2,47 @@
 ## This rudimentary script combines build processes for MariaDB-Tools and backup-manager.
 
 ## Reusables
-URL="https://github.com/mariadb-corporation/mariadb-tools"
+URL="https\:\/\/github.com\/mariadb-corporation\/mariadb-tools"
 
 echo "Welcome to the MariaDB Tools Build Script!"
-echo "Starting Perl Tools Build..."
+echo "Starting Shell Tools Build..."
 
+## Local build of MariaDB-Backup-Manager as defined by Rick Pizzi.
+git submodule sync                                                                                  ## Download the mariadb-backup-manager repository.
+pushd internal-linked-projects/backup-manager                                                       ## new working directory from inside the repo
+cat backup_manager.common backup_manager.packaged backup_manager.main > mariadb-backup-manager      ## thanks Rick.
+chmod 755 mariadb-backup-manager                                                                    ## thanks Rick.
+if [[ $? -gt 0 ]]; then
+  echo "\033[0;31;40mWARN: Could not change permissions for backup-manager. This could prevent the tool from being utilized."
+  echo "\033[0;31;40mCheck *internal-linked-projects/mariadb-backup-manager/mariadb-backup-manager* permissions before continuing"
+fi
+popd
+
+## Consolidate
+mv internal-linked-projects/backup-manager/mariadb-backup-manager bin/
+
+## Checkpoint!
+echo "Shell Tools complete. Beginning Perl tools...."
 
 ## Prep and build the perl binaries.
 perl Makefile.PL
-if [ $? ]; then
-  make install;
+echo perl status: $?
+if [[ $? -eq 0 ]]; then
+  make
 else
   echo "Failed to create Makefile. Please ensure Makefile.PL is readable. Stopping build..."
   exit 2
 fi
 
 ## Success?
-if [ $? ]; then
-  echo "Perl Tools Complete. Moving on to shell utilities..."
+if [[ $? -eq 0 ]]; then
+  echo "Perl Tools Complete. "
 else
   echo "An unaccounted for error occurred during the *make install* execution. Please see the above output for more information."
   exit
 fi
 
-## Local build of MariaDB-Backup-Manager as defined by Rick Pizzi.
-git submodule sync                                                                                  ## Download the mariadb-backup-manager repository.
-pushd internal-linked-projects/mariadb-backup-manager                                               ## new working directory from inside the repo
-cat backup_manager.common backup_manager.packaged backup_manager.main > mariadb-backup-manager      ## thanks Rick.
-chmod 755 mariadb-backup-manager                                                                    ## thanks Rick.
-if [ $? ]; then
-  echo "\033[0;31;40mWARN: Could not change permissions for backup-manager. This could prevent the tool from being utilized.
-  echo "\033[0;31;40mCheck *internal-linked-projects/mariadb-backup-manager/mariadb-backup-manager* permissions before continuing"
-fi
-popd
+make install
 
-## Consolidate
-mv internal-linked-projects/mariadb-backup-manager/mariadb-backup-manager bin/
-
-echo "Tools are available for use in `cwd`/bin."
+echo "Tools are available for use your perl bin location."
 echo "For support or to report an issue with these tools, please visit: $URL"
